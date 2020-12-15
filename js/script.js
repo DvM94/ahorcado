da96315c9a294b0f5756fc72b540322e36039fc2
DvM94 <danielvillarmartin@gmail.com>
2020-12-15 19:17:44 +0100

    actualizacion 15 12 20

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let botonJugarInicio=document.querySelector(".modal-inicio-jugar")
 let botonJugarOpciones=document.querySelector(".modal-opciones-jugar")
 let botonVolverJugar=document.querySelector(".modal-fin-rejugar")
 let botonCerrar=document.querySelector(".marcador-salir")
+let puntuaciones=document.querySelector(".marcador-listado")
 let botonInicio=document.querySelector(".cabecera-menu-inicio")
 let botonPuntuaciones=document.querySelector(".cabecera-menu-puntuacion")
 let temporizador=document.querySelector(".juego-temporizador")
@@ -24,50 +25,70 @@ let partidas=document.querySelector(".juego-datos-partidas")
 //Variables
 
 let seleccion=0
+let fallos=0
+let aciertos=0
+let partidasGanadas=0
 
-/*let jugador={
-  nombre: nombre,
-  partidasGanadas:,
-  fecha : new Date()
-}*/
+let jugador={
+  nombre: "",
+  puntuacion: 0,
+  fecha: new Date()
+}
 
 //Array de temáticas
 
 let palabras=[
-  {palabra:"PERRO",tema:"Animales"},
-  {palabra:"GATO",tema:"Animales"},
-  {palabra:"ESPAÑA",tema:"Países"},
-  {palabra:"AUSTRALIA",tema:"Países"}
+  {palabra:"AB",tema:"Animales"},
+  {palabra:"DEF",tema:"Animales"},
+  {palabra:"GHIJ",tema:"Países"},
+  {palabra:"JKLMN",tema:"Países"}
 ]
 
 //Modal Inicio
 
-botonJugarInicio.onclick=()=>{
+botonJugarInicio.onclick=IniciarPartida
+
+function IniciarPartida(){
+  fallos=0
+  aciertos=0
+  jugador.nombre=nombre.value
+  ahorcado.setAttribute("src","./img/ahorcado0.png")
+  partidas.textContent=`Partidas ganadas: ${partidasGanadas}`
   if(nombre.value=="")
     document.querySelector(".modal-inicio-alerta").classList.remove("oculto")
   else{
     document.querySelector(".modal-inicio").classList.add("oculto")
     nombreJugador.textContent=nombre.value
-    borrarAbecedario()
+    jugador.nombre=nombre.value
+    borrar()
     crearAbecedario()
     seleccion=Math.round(Math.random()*(palabras.length-1))
     tematica.innerHTML=palabras[seleccion].tema
     let palabraElegida=palabras[seleccion].palabra
-    let palabraElegidaOculta=""
-    for(let i=0;i<palabraElegida.length;i++){
-      palabraElegidaOculta+=palabraElegida[i].replace(palabraElegida[i],"_ ")
-    }
-    palabra.innerHTML=palabraElegidaOculta
+    let palabraElegidaArray=Array.from(palabraElegida)
+    let letrasPalabraContenedor=document.createElement("div")
+    let letraPalabra=document.createElement("p")
+    letrasPalabraContenedor.classList.add("borde-inferior")
+    letraPalabra.classList.add("oculto")
+    palabraElegidaArray.forEach(l=>{
+      let p=letraPalabra.cloneNode()
+      p.innerHTML=l
+      let letra=letrasPalabraContenedor.cloneNode()
+      letra.insertAdjacentElement("beforeend",p)
+      palabra.insertAdjacentElement("beforeend",letra)
+    })
+    console.log(palabraElegidaArray)
   }
 }
 
 //Pantalla Principal
 
-function borrarAbecedario(){
-    while(teclado.firstChild){
-      teclado.firstChild.remove()
-    }
-  }
+function borrar(){
+  while(teclado.firstChild)
+    teclado.firstChild.remove()
+  while(palabra.firstChild)
+    palabra.firstChild.remove()
+}
 
   function crearAbecedario(){
     let abecedario=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -80,17 +101,45 @@ function borrarAbecedario(){
         letra.innerHTML=l
         letra.onclick=()=>{
           if(letra.firstElementChild==null){
-            if(palabras[seleccion].palabra.indexOf(letra.textContent)==-1)
+            if(palabras[seleccion].palabra.indexOf(letra.textContent)==-1){
               imgs.setAttribute("src","./img/letraMal.png")
-            else
+              fallos++
+              ahorcado.setAttribute("src","./img/ahorcado"+fallos+".png")
+            }
+            else{
               imgs.setAttribute("src","./img/letraBien.png")
+              let comparacionLetra = Array.from(document.querySelectorAll(".juego-palabra div p"))
+              comparacionLetra.forEach(l=>{
+                if(l.textContent==letra.textContent){
+                  l.classList.remove("oculto")
+                  aciertos++
+                }
+              })           
+            }
+            comprobarJugada()  
             let img=imgs.cloneNode()
             letra.insertAdjacentElement("afterbegin",img)
-            console.log(letra.textContent)
           }
         }
         teclado.appendChild(letra)
     })
+  }
+
+  function comprobarJugada(){
+    if(fallos>=6){
+      teclado.classList.add("invisible")
+      guardarDatos()
+      setTimeout(() => {
+        document.querySelector(".modal-fin").classList.remove("oculto")
+        partidasGanadas=0
+      }, 1500)
+    }
+    if(aciertos==palabras[seleccion].palabra.length){
+      partidasGanadas++
+      setTimeout(() => {
+        IniciarPartida()
+      }, 1500)
+    }
   }
 
 //Botones cabecera
@@ -98,14 +147,43 @@ function borrarAbecedario(){
 botonInicio.onclick=()=>{
   document.querySelector(".modal-inicio").classList.remove("oculto")
   nombre.value=""
+  partidasGanadas=0
 }
 
 botonPuntuaciones.onclick=()=>{
+  puntuaciones.textContent=""
   document.querySelector(".modal-puntuacion").classList.remove("oculto")
+  for(let i=0;i<5;i++){
+    puntuaciones.textContent+=jugadores[i].nombre
+    puntuaciones.textContent+=jugadores[i].puntuacion
+  }
 }
 
 //Modal Puntuaciones
 
 botonCerrar.onclick=()=>{
   document.querySelector(".modal-puntuacion").classList.add("oculto")
+  
 }
+
+function guardarDatos(){
+  jugador.puntuacion=partidasGanadas
+  let puntuaciones=localStorage.getItem("puntuaciones")
+  if(puntuaciones==undefined)
+    jugadores=[]
+  else
+    jugadores=JSON.parse(puntuaciones)
+  jugadores.unshift(jugador)
+  localStorage.setItem("puntuaciones",JSON.stringify(jugadores))
+}
+
+//Modal Perder
+
+botonVolverJugar.onclick=()=>{
+  IniciarPartida()
+  teclado.classList.remove("invisible")
+  document.querySelector(".modal-fin").classList.add("oculto")
+  
+}
+
+
